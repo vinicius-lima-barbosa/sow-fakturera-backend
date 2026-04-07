846546bfc67181b42f1fccfb57af39d200cf04b7
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-04-07 09:49:42 -0300

    feat: add express init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
